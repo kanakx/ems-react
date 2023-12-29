23fa78e79cf1929233e4badf22a0bfc970850b06
kanakx <kacperkanak@icloud.com>
2023-12-29 14:45:30 +0100

    Fix bugs

--- src/pages/AddEventPage.jsx
+++ src/pages/AddEventPage.jsx
@@ -1,30 +1,27 @@
-import {useState} from "react";
 import {useNavigate} from "react-router-dom";
 import EventForm from "../components/EventForm.jsx";
 import {saveEvents} from "../services/eventService.js";
 import {PageTitle} from "../themes/SharedStyles.jsx";
 import PageLayout from "../components/PageLayout.jsx";
+import {toast} from "react-toastify";
 
 const AddEventPage = () => {
     const navigate = useNavigate();
-    const [notification, setNotification] = useState({ message: '', type: '' });
 
     const handleSubmit = (newEventData) => {
         saveEvents(newEventData)
             .then(() => {
-                setNotification({ message: 'Event saved successfully!', type: 'success' });
+                toast.success('Event saved successfully!');
                 setTimeout(() => navigate('/events'), 4000);
             })
             .catch(error => {
                 console.error('Failed to save event: ', error);
-                setNotification({ message: 'Failed to save event.', type: 'error' });
+                toast.error('Failed to save event.');
             });
     };
 
-
     return (
         <PageLayout>
-            {notification.message && <Notification message={notification.message} type={notification.type} />}
             <PageTitle>Add new</PageTitle>
             <EventForm onSubmit={handleSubmit} initialEvent={null} />
         </PageLayout>
