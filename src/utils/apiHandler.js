db3a7b9d6f122874c8655bd084734e05dfb48dc6
kanakx <kacperkanak@icloud.com>
2024-01-07 08:17:43 +0100

    Handle 401 and 403 status codes

--- src/utils/apiHandler.js
+++ src/utils/apiHandler.js
@@ -1,4 +1,4 @@
-import { toast } from "react-toastify";
+import {toast} from "react-toastify";
 
 export const handleApiResponse = response => {
     return response.data;
@@ -9,8 +9,12 @@ export const handleApiError = error => {
     if (error.message === "Network Error" || error.response === undefined) {
         toast.error("Unable to connect to the server. Please try again later.");
     } else {
-        const errorMessage = error.response?.data?.message || 'An unexpected error occurred.';
-        toast.error(errorMessage);
+        if (error.response.status === 401 || error.response.status === 403) {
+            toast.error("No permission to access resource. ");
+        } else {
+            const errorMessage = error.response?.data?.message || 'An unexpected error occurred.';
+            toast.error(errorMessage);
+        }
     }
     throw error;
 };
