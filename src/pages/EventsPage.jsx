179586966eb9b8e7cb1b58f82269f45a47cf9d02
kanakx <kacperkanak@icloud.com>
2024-01-08 13:47:23 +0100

    Clean the comments

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,10 +1,5 @@
-import EventList from "../components/EventList";
-import {useEffect, useState} from "react";
 import {PageSubtitle, PageTitle, StyledButton} from "../themes/SharedStyles.jsx";
 import {useNavigate} from "react-router-dom";
-import {getAllEvents} from "../services/eventService.js";
-import Loading from "../components/Loading.jsx";
-import styled from "styled-components";
 import {useUserContext} from "../contexts/UserContext.jsx";
 import PageLayout from "../components/PageLayout.jsx";
 import EventsPaginator from "../components/EventsPaginator.jsx";
@@ -12,19 +7,7 @@ import EventsPaginator from "../components/EventsPaginator.jsx";
 const EventsPage = () => {
     const navigate = useNavigate();
     const {isAuth, attendee} = useUserContext();
-    const [events, setEvents] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-
-    // useEffect(() => {
-    //     console.log(attendee)
-    //
-    //     setIsLoading(true);
-    //     getAllEvents()
-    //         .then(events => {
-    //             setEvents(events);
-    //         })
-    //         .finally(() => setIsLoading(false));
-    // }, []);
+    const pageSize = 2;
 
     const handleAddNewButton = () => {
         navigate('/events/add');
@@ -34,8 +17,6 @@ const EventsPage = () => {
         navigate('/');
     };
 
-    // if (isLoading) return <Loading onBack={handleBackButton}/>
-
     //TODO maybe some error page or smth?
     if (!attendee) return <div>Please log in to view events</div>
 
@@ -43,13 +24,7 @@ const EventsPage = () => {
         <PageLayout>
             <PageTitle>Events</PageTitle>
             <PageSubtitle>You are invited to</PageSubtitle>
-            {/*{events && events.length > 0 ? (*/}
-            {/*    <EventList events={events}/>*/}
-            {/*) : (*/}
-            {/*    <NoEventsMessage>No events available</NoEventsMessage>*/}
-            {/*)}*/}
-
-            <EventsPaginator pageSize={2}/>
+            <EventsPaginator pageSize={pageSize}/>
 
             {!isAuth && <PageSubtitle>Sign in to add events</PageSubtitle>}
             {isAuth && (
