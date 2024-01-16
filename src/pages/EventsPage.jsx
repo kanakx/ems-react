18fef3316975e2c3e4e1e6b52e4e7413572458a5
kanakx <kacperkanak@icloud.com>
2024-01-16 17:22:58 +0100

    Fix bugs

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -2,11 +2,10 @@ import {PageSubtitle, PageTitle, StyledButton} from "../themes/SharedStyles.jsx"
 import {useNavigate} from "react-router-dom";
 import {useUserContext} from "../contexts/UserContext.jsx";
 import PageLayout from "../components/PageLayout.jsx";
-import EventsPaginator from "../components/EventsPaginator.jsx";
 import {useEffect, useState} from "react";
-import {getAllEvents} from "../services/eventService.js";
 import EventCard from "../components/EventCard.jsx";
 import GenericItemPaginator from "../components/GenericItemPaginator.jsx";
+import {getAttendeeById} from "../services/attendeeService.js";
 
 const EventsPage = () => {
     const navigate = useNavigate();
@@ -14,12 +13,14 @@ const EventsPage = () => {
     const [events, setEvents] = useState([]);
 
     useEffect(() => {
-        getAllEvents()
-            .then(fetchedEvents => {
-                setEvents(fetchedEvents);
-            })
-            .catch(error => console.log(error));
-    }, []);
+        if (attendee && attendee.idAttendee) {
+            getAttendeeById(attendee.idAttendee)
+                .then(fetchedAttendee => {
+                    setEvents(fetchedAttendee.attendeeEventDtoList.map(ae => ae.eventDto));
+                })
+                .catch(error => console.log(error));
+        }
+    }, [attendee]);
 
     if (!attendee) return <div>Please log in to view events</div>
 
@@ -31,13 +32,14 @@ const EventsPage = () => {
                     <StyledButton onClick={() => navigate('/events/add')}>
                         Add new
                     </StyledButton>
-
-                    <GenericItemPaginator
-                        items={events}
-                        pageSize={4}
-                        renderItem={event => <EventCard key={event.idEvent} event={event} />}
-                        noItemsMessage="No events available"
-                    />
+                    {events.length > 0 &&
+                        <GenericItemPaginator
+                            items={events}
+                            pageSize={4}
+                            renderItem={event => <EventCard key={event.idEvent} event={event} />}
+                            noItemsMessage="No events available"
+                        />
+                    }
                 </>
             ) : (
                 <PageSubtitle>Sign in to add events</PageSubtitle>
